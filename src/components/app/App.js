aea4d95a2440418b86d6e1fabd054a64c4c9e85c
Mufolk <mufolk@mbp-de-mufolk.br.ibm.com>
2019-05-22 15:57:48 -0300

    first commit fuck u

---
+++ src/components/app/App.js
@@ -0,0 +1,23 @@
+
+export default {
+    name: 'app',
+    // components {
+      
+    // }
+    data(){
+      return{
+        bundas : [
+          {
+            name: 'b1',
+            index: 1,
+            title: 'bundinha'
+          },
+          {
+            name: 'b2',
+            index: 2,
+            title: 'bundao'
+          }
+        ]
+      }
+    }
+  }
---
+++ src/components/app/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div id="app">
+    <b-nav >
+      <b-nav-item v-for="bunda in bundas" class="comunista">
+        {{ bunda.title }}
+      </b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script src="./App.js"></script>
+<style>
+  @import './App.css';
+</style>
